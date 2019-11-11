4247579ed1ca794924de7a6ac4c741c61e1914ab
Hopson97 <mhopson@hotmail.co.uk>
2019-11-11 16:56:54 +0000

    Entity logic testing

--- src/server/world/entity.cpp
+++ src/server/world/entity.cpp
@@ -29,16 +29,15 @@ namespace server {
 
     void Entity::tick()
     {
-        //std::cout << glm::distance(position, target) << std::endl;
         if (glm::distance(position, target) > 2.0f) {
-            float dx = target.x - position.x;
-            float dz = target.z - position.z;
-            rotation.y = glm::degrees(std::atan2(dz, dx));
             moveRight();
         }
         else {
             target.x = rand() % 32;
             target.z = rand() % 32;
+            float dx = target.x - position.x;
+            float dz = target.z - position.z;
+            rotation.y = glm::degrees(std::atan2(dz, dx));
         }
     }
 } // namespace server
