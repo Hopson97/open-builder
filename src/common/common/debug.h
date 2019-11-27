6d0d027045bfc78dbb514d97fe1ecc6d7357a01e
Hopson97 <mhopson@hotmail.co.uk>
2019-11-27 22:24:15 +0000

    Create debug logger

--- src/common/common/debug.h
+++ src/common/common/debug.h
@@ -2,8 +2,27 @@
 
 #include <SFML/System/Vector3.hpp>
 #include <glm/glm.hpp>
+#include <iostream>
+#include <mutex>
 #include <ostream>
 #include <vector>
+
+//#define DEBUG_MODE_
+
+#ifdef DEBUG_MODE_
+
+#define LOG(title, var)                                                        \
+    std::cout << __FILE__ << ":" << __LINE__ << " - " << title << " " << var       \
+              << std::endl;
+
+#define LOG(item)                                                              \
+    std::cout << __FILE__ << ":" << __LINE__ << " - " << item << std::endl;
+#else
+
+#define LOG(title, var)
+#define LOG()
+
+#endif
 
 template <typename T>
 std::ostream &operator<<(std::ostream &stream, const std::vector<T> &data)
