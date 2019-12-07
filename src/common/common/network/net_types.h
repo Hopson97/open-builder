a623fc1f21e1ce2f420b499c390dbb5bd67c566b
Hopson97 <mhopson@hotmail.co.uk>
2019-12-07 23:05:54 +0000

    Docs for the network types

--- src/common/common/network/net_types.h
+++ src/common/common/network/net_types.h
@@ -5,14 +5,18 @@
 #include <SFML/Network/Packet.hpp>
 #include <SFML/Network/Socket.hpp>
 
-// Network types
+// Port, used for uh ports
 using port_t = u16;
+
+// Client ID, the number used for client naming over the network 
 using client_id_t = u8;
+
+// Command, the numerical representation of a command sent over a network
 using command_t = u16;
 
 /**
-        End point of a connection
-*/
+ * @brief The IP address and Port of an endpoint
+ */
 struct Endpoint {
     sf::IpAddress address;
     port_t port = 0;
