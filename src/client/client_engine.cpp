5cb9eb28ab11c286f8d18d7f977acd0fecdbc931
Hopson97 <mhopson@hotmail.co.uk>
2019-12-09 14:40:55 +0000

    Split client engline decalration and implementation

--- src/client/client_engine.cpp
+++ src/client/client_engine.cpp
@@ -62,8 +62,23 @@ struct Entity {
     bool active = false;
 };
 
-class ClientEngine {
+class ClientEngine final {
+  public:
+    ClientEngine(Window &window);
+    ~ClientEngine();
+
+    EngineStatus run();
+
   private:
+    void input();
+    void update();
+    void render(const glm::mat4& projectionMatrix);
+
+    void handlePackets();
+    void handleSnapshot(Packet &packet);
+    void handlePlayerJoin(Packet &packet);
+    void handlePlayerLeave(Packet &packet);
+
     Window &m_window;
     Keyboard m_keyboard;
 
@@ -80,213 +95,211 @@ class ClientEngine {
     Entity *m_player = nullptr;
 
     bool m_isMouseLocked = false;
-
-  public:
-    ClientEngine(Window &window)
-        : m_window(window)
-    {
-    }
-
-    ~ClientEngine()
-    {
-        m_cube.destroy();
-        m_texture.destroy();
-        m_shader.destroy();
-
-        m_client.disconnect();
-    }
-
-    EngineStatus run()
-    {
-        init();
-
-        glm::mat4 projectionMatrix{1.0f};
-        projectionMatrix =
-            glm::perspective(3.14f / 2.0f, m_window.aspect, 0.01f, 100.0f);
-
-        EngineStatus status = EngineStatus::Ok;
-        sf::Clock frameTimer;
-        int frameCount = 0;
-        while (status == EngineStatus::Ok) {
-            status = m_window.pollEvents(m_keyboard, [this](auto key) {
-                if (key == sf::Keyboard::L) {
-                    m_isMouseLocked = !m_isMouseLocked;
-                }
-            });
-            input();
-            update();
-            render(projectionMatrix);
-
-            // Stats
-            /*
-            frameCount++;
-            if (frameTimer.getElapsedTime().asSeconds() > 2) {
-                float ms = static_cast<float>(
-                    frameTimer.getElapsedTime().asMilliseconds());
-                float secs = frameTimer.getElapsedTime().asSeconds();
-                std::cout << "Average Frame Time: " << ms / frameCount
-                          << "Average FPS: " << frameCount / secs << "\n\n";
-                frameCount = 0;
-                frameTimer.restart();
-            }
-            */
-        }
-        return status;
-    }
-
-  private:
-    void init()
-    {
-        m_cube = createCube();
-
-        m_shader.create("static", "static");
-        m_shader.bind();
-        m_modelLocation = m_shader.getUniformLocation("modelMatrix");
-        m_projectionViewLocation =
-            m_shader.getUniformLocation("projectionViewMatrix");
-
-        m_texture.create("p2");
-        m_texture.bind();
-
-        m_client.connectTo(sf::IpAddress::LocalHost);
-        m_player = &m_entities[m_client.getClientId()];
-    }
-
-    void input()
-    {
-        static auto lastMousepositionition =
-            sf::Mouse::getPosition(m_window.window);
-
-        if (!m_isMouseLocked && m_window.window.hasFocus()) {
-            auto change = sf::Mouse::getPosition(m_window.window) -
-                          lastMousepositionition;
-            m_player->rotation.x += static_cast<float>(change.y / 8.0f);
-            m_player->rotation.y += static_cast<float>(change.x / 8.0f);
-            lastMousepositionition = sf::Mouse::getPosition(m_window.window);
-        }
-
-        const float PLAYER_SPEED = 0.05f;
-        float rads = (glm::radians(m_player->rotation.y));
-        float rads90 = (glm::radians(m_player->rotation.y + 90));
-        if (m_keyboard.isKeyDown(sf::Keyboard::W)) {
-            m_player->position.x -= glm::cos(rads90) * PLAYER_SPEED;
-            m_player->position.z -= glm::sin(rads90) * PLAYER_SPEED;
-        }
-        else if (m_keyboard.isKeyDown(sf::Keyboard::S)) {
-            m_player->position.x += glm::cos(rads90) * PLAYER_SPEED;
-            m_player->position.z += glm::sin(rads90) * PLAYER_SPEED;
-        }
-        if (m_keyboard.isKeyDown(sf::Keyboard::A)) {
-            m_player->position.x -= glm::cos(rads) * PLAYER_SPEED;
-            m_player->position.z -= glm::sin(rads) * PLAYER_SPEED;
-        }
-        else if (m_keyboard.isKeyDown(sf::Keyboard::D)) {
-            m_player->position.x += glm::cos(rads) * PLAYER_SPEED;
-            m_player->position.z += glm::sin(rads) * PLAYER_SPEED;
-        }
-    }
-
-    void update()
-    {
-        auto packet = makePacket(ServerCommand::PlayerPosition);
-        auto id = m_client.getClientId();
-        packet.data << id << m_entities[id].position.x
-                    << m_entities[id].position.y << m_entities[id].position.z;
-        m_client.sendPacketToServer(packet);
-        handlePackets();
-    }
-
-    void render(const glm::mat4 &projectionMatrix)
-    {
-        glm::mat4 viewMatrix{1.0f};
-        rotateMatrix(&viewMatrix, m_player->rotation);
-        translateMatrix(&viewMatrix, -m_player->position);
-
-        glm::mat4 projectionViewMatrix{1.0f};
-        projectionViewMatrix = projectionMatrix * viewMatrix;
-        gl::loadUniform(m_projectionViewLocation, projectionViewMatrix);
-
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        auto drawable = m_cube.getDrawable();
-        drawable.bind();
-        for (auto &p : m_entities) {
-            if (p.active) {
-
-                glm::mat4 modelMatrix{1.0f};
-                translateMatrix(&modelMatrix,
-                                {p.position.x, p.position.y, p.position.z});
-                gl::loadUniform(m_modelLocation, modelMatrix);
-                drawable.draw();
-            }
-        }
-
-        // glm::mat4 modelMatrix{1.0f};
-        // gl::loadUniform(m_modelLocation, modelMatrix);
-        // drawable.draw();
-        m_window.window.display();
-    }
-
-    void handlePackets()
-    {
-        Packet incoming;
-        while (receivePacket(m_client.socket, incoming)) {
-            switch (static_cast<ClientCommand>(incoming.command)) {
-                case ClientCommand::PlayerJoin:
-                    handlePlayerJoin(incoming);
-                    break;
-
-                case ClientCommand::PlayerLeave:
-                    handlePlayerLeave(incoming);
-                    break;
-
-                case ClientCommand::Snapshot:
-                    handleSnapshot(incoming);
-                    break;
-
-                case ClientCommand::ConnectionChallenge:
-                case ClientCommand::AcceptConnection:
-                case ClientCommand::RejectConnection:
-                    break;
-            }
-        }
-    }
-
-    void handleSnapshot(Packet &packet)
-    {
-        u16 n = 0;
-        packet.data >> n;
-
-        for (u16 i = 0; i < n; i++) {
-            client_id_t id = 0;
-            float x, y, z;
-            packet.data >> id >> x >> y >> z;
-            if (id != m_client.getClientId()) {
-                auto *p = &m_entities[id];
-                p->position = {x, y, z};
-                p->active = true;
-            }
-        }
-    }
-
-    void handlePlayerJoin(Packet &packet)
-    {
-        client_id_t id = 0;
-        packet.data >> id;
-        m_entities[id].active = true;
-        LOGVAR("Client", "Player has joined the game, Client ID:", (int)id);
-    }
-
-    void handlePlayerLeave(Packet &packet)
-    {
-        client_id_t id = 0;
-        packet.data >> id;
-        m_entities[id].active = false;
-        LOGVAR("Client", "Player has left the game, Client ID:", (int)id);
-    }
 };
 
+ClientEngine::ClientEngine(Window &window)
+    : m_window(window)
+{
+}
+
+ClientEngine::~ClientEngine()
+{
+    m_cube.destroy();
+    m_texture.destroy();
+    m_shader.destroy();
+
+    m_client.disconnect();
+}
+
+EngineStatus ClientEngine::run()
+{
+    m_cube = createCube();
+
+    m_shader.create("static", "static");
+    m_shader.bind();
+    m_modelLocation = m_shader.getUniformLocation("modelMatrix");
+    m_projectionViewLocation =
+        m_shader.getUniformLocation("projectionViewMatrix");
+
+    m_texture.create("p2");
+    m_texture.bind();
+
+    m_client.connectTo(sf::IpAddress::LocalHost);
+    m_player = &m_entities[m_client.getClientId()];
+
+    glm::mat4 projectionMatrix{1.0f};
+    projectionMatrix =
+        glm::perspective(3.14f / 2.0f, m_window.aspect, 0.01f, 100.0f);
+
+    EngineStatus status = EngineStatus::Ok;
+    sf::Clock frameTimer;
+    int frameCount = 0;
+    while (status == EngineStatus::Ok) {
+        status = m_window.pollEvents(m_keyboard, [this](auto key) {
+            if (key == sf::Keyboard::L) {
+                m_isMouseLocked = !m_isMouseLocked;
+            }
+        });
+        input();
+        update();
+        render(projectionMatrix);
+
+        // Stats
+        /*
+        frameCount++;
+        if (frameTimer.getElapsedTime().asSeconds() > 2) {
+            float ms = static_cast<float>(
+                frameTimer.getElapsedTime().asMilliseconds());
+            float secs = frameTimer.getElapsedTime().asSeconds();
+            std::cout << "Average Frame Time: " << ms / frameCount
+                      << "Average FPS: " << frameCount / secs << "\n\n";
+            frameCount = 0;
+            frameTimer.restart();
+        }
+        */
+    }
+    return status;
+}
+
+void ClientEngine::input()
+{
+    static auto lastMousepositionition =
+        sf::Mouse::getPosition(m_window.window);
+
+    if (!m_isMouseLocked && m_window.window.hasFocus()) {
+        auto change =
+            sf::Mouse::getPosition(m_window.window) - lastMousepositionition;
+        m_player->rotation.x += static_cast<float>(change.y / 8.0f);
+        m_player->rotation.y += static_cast<float>(change.x / 8.0f);
+        lastMousepositionition = sf::Mouse::getPosition(m_window.window);
+    }
+
+    const float PLAYER_SPEED = 0.05f;
+    float rads = (glm::radians(m_player->rotation.y));
+    float rads90 = (glm::radians(m_player->rotation.y + 90));
+    if (m_keyboard.isKeyDown(sf::Keyboard::W)) {
+        m_player->position.x -= glm::cos(rads90) * PLAYER_SPEED;
+        m_player->position.z -= glm::sin(rads90) * PLAYER_SPEED;
+    }
+    else if (m_keyboard.isKeyDown(sf::Keyboard::S)) {
+        m_player->position.x += glm::cos(rads90) * PLAYER_SPEED;
+        m_player->position.z += glm::sin(rads90) * PLAYER_SPEED;
+    }
+    if (m_keyboard.isKeyDown(sf::Keyboard::A)) {
+        m_player->position.x -= glm::cos(rads) * PLAYER_SPEED;
+        m_player->position.z -= glm::sin(rads) * PLAYER_SPEED;
+    }
+    else if (m_keyboard.isKeyDown(sf::Keyboard::D)) {
+        m_player->position.x += glm::cos(rads) * PLAYER_SPEED;
+        m_player->position.z += glm::sin(rads) * PLAYER_SPEED;
+    }
+}
+
+void ClientEngine::update()
+{
+    auto packet = makePacket(ServerCommand::PlayerPosition);
+    auto id = m_client.getClientId();
+    packet.data << id << m_entities[id].position.x << m_entities[id].position.y
+                << m_entities[id].position.z;
+    m_client.sendPacketToServer(packet);
+    handlePackets();
+}
+
+void ClientEngine::render(const glm::mat4 &projectionMatrix)
+{
+    glm::mat4 viewMatrix{1.0f};
+    rotateMatrix(&viewMatrix, m_player->rotation);
+    translateMatrix(&viewMatrix, -m_player->position);
+
+    glm::mat4 projectionViewMatrix{1.0f};
+    projectionViewMatrix = projectionMatrix * viewMatrix;
+    gl::loadUniform(m_projectionViewLocation, projectionViewMatrix);
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    auto drawable = m_cube.getDrawable();
+    drawable.bind();
+    for (auto &p : m_entities) {
+        if (p.active) {
+
+            glm::mat4 modelMatrix{1.0f};
+            translateMatrix(&modelMatrix,
+                            {p.position.x, p.position.y, p.position.z});
+            gl::loadUniform(m_modelLocation, modelMatrix);
+            drawable.draw();
+        }
+    }
+
+    // glm::mat4 modelMatrix{1.0f};
+    // gl::loadUniform(m_modelLocation, modelMatrix);
+    // drawable.draw();
+    m_window.window.display();
+}
+
+
+//  =   =   =   =   =   =   
+//  Packet Handling Code
+//  =   =   =   =   =   =
+void ClientEngine::handlePackets()
+{
+    Packet incoming;
+    while (receivePacket(m_client.socket, incoming)) {
+        switch (static_cast<ClientCommand>(incoming.command)) {
+            case ClientCommand::PlayerJoin:
+                handlePlayerJoin(incoming);
+                break;
+
+            case ClientCommand::PlayerLeave:
+                handlePlayerLeave(incoming);
+                break;
+
+            case ClientCommand::Snapshot:
+                handleSnapshot(incoming);
+                break;
+
+            case ClientCommand::ConnectionChallenge:
+            case ClientCommand::AcceptConnection:
+            case ClientCommand::RejectConnection:
+                break;
+        }
+    }
+}
+
+void ClientEngine::handleSnapshot(Packet &packet)
+{
+    u16 n = 0;
+    packet.data >> n;
+
+    for (u16 i = 0; i < n; i++) {
+        client_id_t id = 0;
+        float x, y, z;
+        packet.data >> id >> x >> y >> z;
+        if (id != m_client.getClientId()) {
+            auto *p = &m_entities[id];
+            p->position = {x, y, z};
+            p->active = true;
+        }
+    }
+}
+
+void ClientEngine::handlePlayerJoin(Packet &packet)
+{
+    client_id_t id = 0;
+    packet.data >> id;
+    m_entities[id].active = true;
+    LOGVAR("Client", "Player has joined the game, Client ID:", (int)id);
+}
+
+void ClientEngine::handlePlayerLeave(Packet &packet)
+{
+    client_id_t id = 0;
+    packet.data >> id;
+    m_entities[id].active = false;
+    LOGVAR("Client", "Player has left the game, Client ID:", (int)id);
+}
+
 } // namespace
+
 
 EngineStatus runClientEngine(const ClientConfig &config)
 {
