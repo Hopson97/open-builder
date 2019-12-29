274f71c1098208f540d31106be818b679dd58429
Hopson97 <mhopson@hotmail.co.uk>
2019-12-29 14:04:49 +0000

    Add additional tests

--- tests/common/world/coordinate_test.cpp
+++ tests/common/world/coordinate_test.cpp
@@ -19,7 +19,11 @@ TEST_CASE("Coordinate systems can be correctly converted between")
 
         position = {-1, CHUNK_SIZE, CHUNK_SIZE};
         converted = {-1, 1, 1};
-        REQUIRE(toChunkPosition(position).x == converted.x);
+        REQUIRE(toChunkPosition(position) == converted);
+
+        position = {-1, -CHUNK_SIZE * 2 + 5, CHUNK_SIZE};
+        converted = {-1, -2, 1};
+        REQUIRE(toChunkPosition(position) == converted );
     }
 
     SECTION("Block positions can be correctly converted to an array index")
@@ -53,4 +57,68 @@ TEST_CASE("Coordinate systems can be correctly converted between")
         converted = {CHUNK_SIZE - 1, 0, 0};
         REQUIRE(toLocalBlockPosition(position) == converted);
     }
+
+    SECTION("World positions can be converted to chunk positions")
+    {
+        {
+            ChunkPosition position(5, 6, 2);
+            glm::vec3 worldPosition({CHUNK_SIZE * position.x, CHUNK_SIZE * position.y, CHUNK_SIZE * position.z});
+
+            REQUIRE(worldToChunkPosition(worldPosition) == position);
+        }
+        {
+            ChunkPosition position(-5, -6, -2);
+            glm::vec3 worldPosition({CHUNK_SIZE * position.x, CHUNK_SIZE * position.y, CHUNK_SIZE * position.z});
+
+            REQUIRE(worldToChunkPosition(worldPosition).x + 1 == position.x);
+            REQUIRE(worldToChunkPosition(worldPosition).y + 1 == position.y);
+            REQUIRE(worldToChunkPosition(worldPosition).z + 1 == position.z);
+        }
+        {
+            ChunkPosition position(-5, 60, -2);
+            glm::vec3 worldPosition({CHUNK_SIZE * position.x, CHUNK_SIZE * position.y, CHUNK_SIZE * position.z});
+
+            REQUIRE(worldToChunkPosition(worldPosition).x + 1 == position.x);
+            REQUIRE(worldToChunkPosition(worldPosition).y == position.y);
+            REQUIRE(worldToChunkPosition(worldPosition).z + 1 == position.z);
+        }
+    }
+
+    SECTION("World positions can be converted to global block positions")
+    {
+        {
+            float x = 10;
+            float y = 23212;
+            float z = 1322;
+            
+            glm::vec3 worldPosition(x, y, z);
+
+            REQUIRE(toBlockPosition(worldPosition).x == static_cast<i32>(x));
+            REQUIRE(toBlockPosition(worldPosition).y == static_cast<i32>(y));
+            REQUIRE(toBlockPosition(worldPosition).z == static_cast<i32>(z));
+        }
+        {
+            float x = -10;
+            float y = -23212;
+            float z = -1322;
+            
+            glm::vec3 worldPosition(x, y, z);
+
+            REQUIRE(toBlockPosition(worldPosition).x == static_cast<i32>(x));
+            REQUIRE(toBlockPosition(worldPosition).y == static_cast<i32>(y));
+            REQUIRE(toBlockPosition(worldPosition).z == static_cast<i32>(z));
+        }
+        {
+            float x = -10465;
+            float y = 422;
+            float z = 13222;
+            
+            glm::vec3 worldPosition(x, y, z);
+
+            REQUIRE(toBlockPosition(worldPosition).x == static_cast<i32>(x));
+            REQUIRE(toBlockPosition(worldPosition).y == static_cast<i32>(y));
+            REQUIRE(toBlockPosition(worldPosition).z == static_cast<i32>(z));
+        }
+    }
+
 }
