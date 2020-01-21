4a2cb3a51f83e79d776917f5d0b9dc8675649b2f
Hopson97 <mhopson@hotmail.co.uk>
2020-01-21 22:40:38 +0000

    Create batch script to create a depolyable folder with the bins

---
+++ scripts/CreateDeployable.bat
@@ -0,0 +1,24 @@
+rmdir /s /q OpenBuilder
+mkdir OpenBuilder
+mkdir OpenBuilder\game
+mkdir OpenBuilder\shaders
+mkdir OpenBuilder\res
+mkdir OpenBuilder\texture_packs
+
+
+xcopy /s .\Release\open-builder.exe OpenBuilder
+
+xcopy /s /e .\game OpenBuilder\game
+xcopy /s /e .\shaders OpenBuilder\shaders
+xcopy /s /e .\res OpenBuilder\res
+xcopy /s /e .\texture_packs OpenBuilder\texture_packs
+
+robocopy .\ OpenBuilder credits.txt
+robocopy .\ OpenBuilder config.obd
+
+robocopy .\ OpenBuilder sfml-audio-2.dll
+robocopy .\ OpenBuilder sfml-graphics-2.dll
+robocopy .\ OpenBuilder sfml-network-2.dll
+robocopy .\ OpenBuilder sfml-system-2.dll
+robocopy .\ OpenBuilder sfml-window-2.dll
+robocopy .\ OpenBuilder openal32.dll
