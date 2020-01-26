139bb5ff224a516e86452163012a2e2f28ec59b8
Hopson97 <mhopson@hotmail.co.uk>
2020-01-26 19:03:59 +0000

    Adds some documentation/comments to the server data classes

--- src/server/data/server_biome_data.h
+++ src/server/data/server_biome_data.h
@@ -7,6 +7,10 @@
 
 class VoxelData;
 
+/**
+ * @brief Inputs for the noise functions
+ *  Used for biome map generation, and height map generation
+ */
 struct NoiseParameters {
     int octaves = 0;
     float amplitude = 0;
@@ -15,6 +19,10 @@ struct NoiseParameters {
     float offset = 0;
 };
 
+/**
+ * @brief Data about an individual biome
+ * These are created via the Lua, see game/biome_types.lua for examples
+ */
 struct BiomeDefinition {
     NoiseParameters primaryNoise;
     NoiseParameters secondaryNoise;
@@ -30,15 +38,48 @@ struct BiomeDefinition {
     block_t beachVoxel = 0;
 };
 
+/**
+ * @brief Container class for different biome types
+ */
 class BiomeData {
   public:
     BiomeData(const VoxelData &voxelData);
+
     void addBiome(const sol::table &table);
+
+    /**
+     * @brief Set the up the "biome map"
+     * This is set from the Lua code, and describes where each biome is in the
+     * world using the "biome noise map".
+     * @param table Lua table containing the biome map information
+     */
     void setBiomeMapper(const sol::table &table);
 
+    /**
+     * @brief Get the biome ID from a point in the biome map
+     * Used for terrain generation, this function will select the biome to use
+     * based on 2 things
+     * 1. The biome mapper, set from Lua
+     * 2. The biome map noise value
+     * @param value Noise value from the biome map
+     * @return int The BiomeId of the biome for the inputted noise value
+     */
     int getBiomeIdFromMapper(float value) const;
 
+    /**
+     * @brief Get the BiomeId based on a biome name string
+     *
+     * @param biomeName The biome name
+     * @return int The biome ID for that biome
+     */
     int getBiomeId(const std::string &biomeName) const;
+
+    /**
+     * @brief Get the BiomeDefintion for that BiomeId
+     * 
+     * @param biomeId The biome ID
+     * @return const BiomeDefinition& The data of that biome
+     */
     const BiomeDefinition &getBiomeData(int biomeId) const;
 
   private:
--- src/server/data/server_voxel_data.h
+++ src/server/data/server_voxel_data.h
@@ -7,6 +7,10 @@
 
 #include <common/world/world_constants.h>
 
+/**
+ * @brief Information about a type of voxel in the world
+ * This is set via the Lua, see game/voxel_types.lua for examples
+ */
 struct ServerVoxel {
     std::string name;
     std::string topTexture;
@@ -23,6 +27,10 @@ class VoxelData {
   public:
     void addVoxel(const sol::table &table);
 
+    /**
+     * @brief Returns all voxels for serialisation purposes
+     * @return const std::vector<ServerVoxel>& The voxel data
+     */
     const std::vector<ServerVoxel> &voxelData() const;
 
     block_t getVoxelId(std::string &voxelName) const;
