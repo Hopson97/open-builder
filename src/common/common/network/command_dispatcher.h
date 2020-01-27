3a54522f4fd9e2c00120018c765b9bf1b171ab1e
Hopson97 <mhopson@hotmail.co.uk>
2020-01-27 22:16:58 +0000

    Add command dispatcher class

---
+++ src/common/common/network/command_dispatcher.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <SFML/Network/Packet.hpp>
+#include <vector>
+#include "net_types.h"
+
+template <typename Handler, typename CommandType>
+class CommandDispatcher {
+  public:
+    using CommandHandler = void (Handler::*)(sf::Packet &packet);
+
+    CommandDispatcher()
+    :   m_commands  (static_cast<size_t>(CommandType::COUNT))
+    {
+        
+    }
+
+    void addCommand(CommandType command, CommandHandler handler)
+    {
+        m_commands[static_cast<command_t>(command)] = handler;
+    }
+
+    void execute(Handler& handler, sf::Packet& packet)
+    {
+        command_t command;
+        packet >> command;
+        (handler.*(m_commands[command]))(packet);
+    }
+
+  private:
+    std::vector<CommandHandler> m_commands;
+};
