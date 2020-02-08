8832fc9341aa637a691c913ecc83cc21c7c5dad7
Hopson97 <mhopson@hotmail.co.uk>
2020-02-08 15:29:33 +0000

    [Deset biome] Improve the look of cactus gen

--- game/world/common_cactus_gen.lua
+++ game/world/common_cactus_gen.lua
@@ -1,9 +1,25 @@
 local cactus = game.data.getVoxel("openbuilder_common_cactus")
 
+local function createCactusColumn(chunk, bx, by, bz, height)
+    for y = 0, height, 1 do
+        chunk:setBlock(bx, y + by, bz, cactus)
+    end
+end
+
 function createCactus(chunk, bx, by, bz)
     local height = 5
 
-    for y = 0, height, 1 do
-        chunk:setBlock(bx, y + by, bz, cactus)
-     end
+    createCactusColumn(chunk, bx, by, bz, height)
+
+    if (bx + bz % 2 == 0) then
+        chunk:setBlock(bx - 1, by + 4, bz, cactus)
+        chunk:setBlock(bx + 1, by + 3, bz, cactus)
+    else
+        chunk:setBlock(bx, by + 4, bz - 1, cactus)
+        chunk:setBlock(bx, by + 3, bz + 1, cactus)
+    end
+
+    
+
+
 end
