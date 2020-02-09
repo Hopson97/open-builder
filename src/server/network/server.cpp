812a9293ff10aaf629ea464475f219e75827ba47
Hopson97 <mhopson@hotmail.co.uk>
2020-02-09 10:13:26 +0000

    [Lua] Expose rng class to Lua

--- src/server/network/server.cpp
+++ src/server/network/server.cpp
@@ -27,6 +27,7 @@ Server::Server(const ServerConfig& config)
     // (Stuff that Lua calls that is defined on the C++ side)
     luaInitDataApi(m_script, m_biomeData, m_voxelData);
     luaInitWorldApi(m_script);
+    luaInitUtilApi(m_script);
 
     // Load game in this order 
     // Voxels then Biomes
