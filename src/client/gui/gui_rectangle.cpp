1baa754f60fba732da43aaf379ffd3bf5b426ab7
Hopson97 <mhopson@hotmail.co.uk>
2020-02-13 22:51:26 +0000

    [GUI Rectangle] Add function to create a model matrix transform for the gui rectangle class

--- src/client/gui/gui_rectangle.cpp
+++ src/client/gui/gui_rectangle.cpp
@@ -1,5 +1,7 @@
 #include "gui_rectangle.h"
 
+#include <glm/gtc/matrix_transform.hpp>
+
 GuiDimension::GuiDimension(float xScale, float xOffset, float yScale, float yOffset)
     : scale{xScale, yScale}
     , offset{xOffset, yOffset}
@@ -12,4 +14,20 @@ glm::vec2 GuiDimension::apply(float width, float height) const
     vector.x = scale.x * width * 100.0f;
     vector.y = scale.y * height * 100.0f;
     return vector + offset;
+}
+
+GuiRectangle::GuiRectangle(const GuiDimension& position, const GuiDimension& scale)
+    : m_position(position)
+    , m_scale(scale)
+{
+}
+
+glm::mat4 GuiRectangle::getRenderTransform(const glm::vec2& viewport) const
+{
+    glm::vec2 position = m_position.apply(viewport.x, viewport.y);
+    glm::vec2 scale = m_scale.apply(viewport.x, viewport.y);
+
+    glm::mat4 modelMatrix{1.0f};
+    modelMatrix = glm::translate(modelMatrix, {position.x, position.y, 0.0f});
+    modelMatrix = glm::scale(modelMatrix, {scale.x, scale.y, 0.0f});
 }
