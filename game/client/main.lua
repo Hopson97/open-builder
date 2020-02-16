16ecf7529fd0e8af7c0de4b9d86d9849234ca071
Hopson97 <mhopson@hotmail.co.uk>
2020-02-16 09:42:27 +0000

    [Gui] Remove testing elements from the lua

--- game/client/main.lua
+++ game/client/main.lua
@@ -7,17 +7,17 @@ crosshair.position = GuiDim.new(0.5, -16, 0.5, -16)
 crosshair.size = GuiDim.new(0, 32, 0, 32)
 crosshair.texture = texCrosshair
 
-local hotbar = hud:addRect()
-hotbar.position = GuiDim.new(0.25, 0, 0.01, 0)
-hotbar.size = GuiDim.new(0.5, 0, 0.1, 0)
-
-local textTest = hud:addText()
-textTest.position = GuiDim.new(0.25, 0, 0.01, 0)
-textTest.size = 50
-textTest.text = "Hello world"
-
-
-local textTest2 = hud:addText()
-textTest2.position = GuiDim.new(0.25, 0, 0.21, 0)
-textTest2.size = 50
-textTest2.text = "Hello world"
+--local hotbar = hud:addRect()
+--hotbar.position = GuiDim.new(0.25, 0, 0.01, 0)
+--hotbar.size = GuiDim.new(0.5, 0, 0.1, 0)
+--
+--local textTest = hud:addText()
+--textTest.position = GuiDim.new(0.25, 0, 0.01, 0)
+--textTest.size = 50
+--textTest.text = "Hello world"
+--
+--
+--local textTest2 = hud:addText()
+--textTest2.position = GuiDim.new(0.25, 0, 0.21, 0)
+--textTest2.size = 50
+--textTest2.text = "Hello world"
