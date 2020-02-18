aba62b6eca56084e588c867d6c59dfdf4004778d
Hopson97 <mhopson@hotmail.co.uk>
2020-02-18 21:45:08 +0000

    [Client] Run the lua scirpt to show main menu stuff at start of game

--- game/client/main.lua
+++ game/client/main.lua
@@ -2,22 +2,39 @@
 local texCrosshair = game.gui.getTexture("res/crosshair.png")
 local hud = game.gui.makeGui()
 
+local W = 0.2
+local H = 0.3
+
+local X = 0.085
+local Y = 0.01
+local GAP = W + 0.01
+
 local crosshair = hud:addRect()
 crosshair.position = GuiDim.new(0.5, -16, 0.5, -16)
 crosshair.size = GuiDim.new(0, 32, 0, 32)
 crosshair.texture = texCrosshair
 
---local hotbar = hud:addRect()
---hotbar.position = GuiDim.new(0.25, 0, 0.01, 0)
---hotbar.size = GuiDim.new(0.5, 0, 0.1, 0)
---
---local textTest = hud:addText()
---textTest.position = GuiDim.new(0.25, 0, 0.01, 0)
---textTest.size = 50
---textTest.text = "Hello world"
---
---
---local textTest2 = hud:addText()
---textTest2.position = GuiDim.new(0.25, 0, 0.21, 0)
---textTest2.size = 50
---textTest2.text = "Hello world"
+local logo = hud:addRect()
+logo.position = GuiDim.new(0.01, 0, 0.85 -Y, 0)
+logo.size = GuiDim.new(W * 2, 0, W / 2, 50)
+logo.texture = game.gui.getTexture("res/logo.png")
+
+local spButtonRect = hud:addRect()
+spButtonRect.position = GuiDim.new(X, 0, Y, 0)
+spButtonRect.size = GuiDim.new(W, 0, H, 0)
+X = X + GAP
+
+local mpButtonrect = hud:addRect()
+mpButtonrect.position = GuiDim.new(X, 0, Y, 0)
+mpButtonrect.size = GuiDim.new(W, 0, H, 0)
+X = X + GAP
+
+local settingsButtonRect = hud:addRect()
+settingsButtonRect.position = GuiDim.new(X, 0, Y, 0)
+settingsButtonRect.size = GuiDim.new(W, 0, H, 0)
+X = X + GAP
+
+local exitButtonRect = hud:addRect()
+exitButtonRect.position = GuiDim.new(X, 0, Y, 0)
+exitButtonRect.size = GuiDim.new(W, 0, H, 0)
+X = X + GAP
