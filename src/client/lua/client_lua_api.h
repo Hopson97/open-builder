06742e1a5efe68c553d0a1a868fec965a16168e3
Hopson97 <mhopson@hotmail.co.uk>
2020-02-19 19:26:04 +0000

    [Client] Add callback support to client, for client startup

--- src/client/lua/client_lua_api.h
+++ src/client/lua/client_lua_api.h
@@ -2,5 +2,7 @@
 
 class ScriptEngine;
 class GuiMaster;
+struct ClientLuaCallbacks;
 
 void initGuiApi(ScriptEngine& engine, GuiMaster& guiMaster);
+void initClientCallbackApi(ScriptEngine& engine, ClientLuaCallbacks& callbacks);
