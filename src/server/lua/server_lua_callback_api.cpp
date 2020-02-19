cb12dd4dd33a30fd6b5dda5281dd2fa7eafe1758
Hopson97 <mhopson@hotmail.co.uk>
2020-02-19 19:57:41 +0000

    [Server] Add Lua API for registering a callback function

---
+++ src/server/lua/server_lua_callback_api.cpp
@@ -0,0 +1,11 @@
+#include "server_lua_api.h"
+
+#include "server_lua_callback.h"
+#include <common/scripting/script_engine.h>
+
+void luaInitServerCallbackApi(ScriptEngine& script, ServerLuaCallbacks& callbacks)
+{
+    script.gameTable["onPlayerJoin"] = [&callbacks](sol::function f) {
+        callbacks.onPlayerJoinCallbacks.push_back(f);
+    };
+}
