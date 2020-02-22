2d145d6ccc28003aceb2c4522eb57c3acf8b4539
Hopson97 <mhopson@hotmail.co.uk>
2020-02-22 15:17:25 +0000

    [GUI] Add doc/comments to the classes of the new gui system

--- src/client/gui/overlay.h
+++ src/client/gui/overlay.h
@@ -8,7 +8,10 @@
 #include <vector>
 
 namespace gui {
-
+/**
+ * @brief Defines how a GUI should be created.
+ * This is created in the Client Lua code
+ */
 struct OverlayDefinition final {
     std::string id;
     std::string title;
@@ -16,6 +19,10 @@ struct OverlayDefinition final {
     sol::function create;
 };
 
+/**
+ * @brief Holds the components that make up some GUI overlay, eg HUD, menus etc
+ *
+ */
 class Overlay final {
   public:
     Overlay(const OverlayDefinition& overlayDefinition);
@@ -24,10 +31,21 @@ class Overlay final {
     void handleMouseMove(sf::Event::MouseMoveEvent);
     void handleKeyRelease(sf::Keyboard::Key);
 
+    /**
+     * @brief Adds a new image the overlay
+     * @return ImageWidget* Non-Owning pointer to the new image widget created
+     */
     ImageWidget* addImage();
+
+    /**
+     * @brief Adds a new label the overlay
+     * @return LabelWidget* Non-Owning pointer to the new label widget created
+     */
     LabelWidget* addLabel();
 
+    // The overlay defintion that created this overlay
     const OverlayDefinition& definition;
+
     // Stored as pointers to allow them to be stored by the Lua code
     std::vector<std::unique_ptr<RectangleComponent>> rectangleComponents;
     std::vector<std::unique_ptr<TextComponent>> textComponents;
@@ -38,6 +56,9 @@ class Overlay final {
     std::vector<std::unique_ptr<Widget>> m_widgets;
 };
 
+/**
+ * @brief Stores the current overlays of the game
+ */
 struct OverlayStack final {
     void pushLayer(std::unique_ptr<Overlay>);
     void popLayer();
@@ -50,9 +71,25 @@ struct OverlayStack final {
     std::vector<std::unique_ptr<Overlay>> overlays;
 };
 
+/**
+ * @brief Stores overlay defintions, and creates overlays using them
+ *
+ */
 class OverlayFactory {
   public:
+    /**
+     * @brief Add a new overlay definition to the game
+     * @param overlay The new overlay
+     */
     void addOverlay(const OverlayDefinition& overlay);
+
+    /**
+     * @brief Create a Overlay object from a given overlay definition
+     *
+     * @param name The "overlay definition id", as defined in the Lua code
+     * @return std::unique_ptr<Overlay> A newly created overlay, as a pointer such that
+     * the Lua is able to store it without risk
+     */
     std::unique_ptr<Overlay> createOverlay(const std::string& name);
 
   private:
