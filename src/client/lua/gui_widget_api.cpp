f3066e06b1d555186557f8f9a32ec79d5db8eab2
Hopson97 <mhopson@hotmail.co.uk>
2020-02-22 13:19:37 +0000

    Add GUI Dimension api

--- src/client/lua/gui_widget_api.cpp
+++ src/client/lua/gui_widget_api.cpp
@@ -1,21 +1,27 @@
 #include "client_lua_api.h"
 
 #include "../gui/widget.h"
+#include "../gui/component.h"
 #include <common/scripting/script_engine.h>
 
-namespace 
+namespace {
+    void addGuiDimensionApi(ScriptEngine& scriptEngine)
 {
-    void addGuiImageApi(ScriptEngine& engine)
-    {
-        auto imageWidgetApi = engine.lua.new_usertype<gui::ImageWidget>("ImageWidget");
-        imageWidgetApi["position"] = sol::property(&gui::ImageWidget::setPosition);
-        imageWidgetApi["size"] = sol::property(&gui::ImageWidget::setSize);
+    auto gdimApi = scriptEngine.lua.new_usertype<gui::GuiDimension>(
+        "GuiDim", sol::constructors<gui::GuiDimension(float, float, float, float)>());
+}
+void addGuiImageApi(ScriptEngine& engine)
+{
+    auto imageWidgetApi = engine.lua.new_usertype<gui::ImageWidget>("ImageWidget");
+    imageWidgetApi["position"] = sol::property(&gui::ImageWidget::setPosition);
+    imageWidgetApi["size"] = sol::property(&gui::ImageWidget::setSize);
 
-    }
-} // namespace 
-
+    imageWidgetApi["image"] = sol::property(&gui::ImageWidget::setImage);
+}
+} // namespace
 
 void luaInitGuiWidgetApi(ScriptEngine& scriptEngine)
 {
+    addGuiDimensionApi(scriptEngine);
     addGuiImageApi(scriptEngine);
 }
