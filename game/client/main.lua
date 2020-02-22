45ba452d5c55765bb02ba7d35023639b7a6eb3f9
Hopson97 <mhopson@hotmail.co.uk>
2020-02-22 19:09:25 +0000

    [Lua] Create main menu v1

--- game/client/main.lua
+++ game/client/main.lua
@@ -1,32 +1,5 @@
 
-local crosshairTexture = game.gui.getTexture("res/crosshair.png")
-
-local function createHud(overlay)
-    local crosshair = overlay:addImage()
-    crosshair.image = crosshairTexture
-    crosshair.position = GuiDim.new(0.5, -16, 0.5, -16)
-    crosshair.size = GuiDim.new(0, 32, 0, 32)
-
-    local label = overlay:addLabel()
-    label.text = "hello"
-    label.textSize = 50
-    label.position = GuiDim.new(0.1, -16, 0.9, -16)
-
-    local button = overlay:addButton()
-    button.text = "hello"
-    button.textSize = 50
-    button.position = GuiDim.new(0.6, 0, 0.6, 0)
-    button.size = GuiDim.new(0.2, 0, 0.1, 0)
-    button.onClick = function() print("Clicked") end
-
-end 
-
-game.gui.addGui{
-    id = "hud",
-    title = "Player HUD",
-    create = createHud
-}
-
-game.onClientStartup(function() 
-    game.gui.push("hud")
-end)
+--
+--  Menus
+--
+dofile("game/client/main_menu.lua")
