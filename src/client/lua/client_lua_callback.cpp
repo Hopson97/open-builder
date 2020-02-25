4ece4cc9b16f53f069b496d78f6d46826796f24f
Hopson97 <mhopson@hotmail.co.uk>
2020-02-25 21:08:48 +0000

    [Lua Callback] Add callbacks for sfml key releases

--- src/client/lua/client_lua_callback.cpp
+++ src/client/lua/client_lua_callback.cpp
@@ -5,9 +5,19 @@
 ClientLuaCallbacks::ClientLuaCallbacks(ScriptEngine& scriptEngine)
 {
     scriptEngine.addCallbackApi("onClientStartup", m_onClientStartupCallbacks);
+
+    scriptEngine.addFunction("onKeyReleased",
+                             [this](sf::Keyboard::Key key, sol::function f) {
+                                 m_onKeyReleaseCallbacks[key].push_back(f);
+                             });
 }
 
 void ClientLuaCallbacks::onClientStartup()
 {
     runLuaCallbacks(m_onClientStartupCallbacks);
+}
+
+void ClientLuaCallbacks::onKeyboardKeyReleased(sf::Keyboard::Key key)
+{
+    runLuaCallbacks(m_onKeyReleaseCallbacks[key]);
 }
--- src/client/lua/client_lua_callback.h
+++ src/client/lua/client_lua_callback.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <SFML/Window/Keyboard.hpp>
+#include <array>
 #include <sol/sol.hpp>
 #include <vector>
 
@@ -10,7 +12,10 @@ class ClientLuaCallbacks {
     ClientLuaCallbacks(ScriptEngine& scriptEngine);
 
     void onClientStartup();
+    void onKeyboardKeyReleased(sf::Keyboard::Key key);
 
   private:
     std::vector<sol::function> m_onClientStartupCallbacks;
+    std::array<std::vector<sol::function>, sf::Keyboard::KeyCount>
+        m_onKeyReleaseCallbacks;
 };
