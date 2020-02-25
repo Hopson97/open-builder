d4d58cd60ffe298d8d638abf03feda0112843533
Hopson97 <mhopson@hotmail.co.uk>
2020-02-25 19:41:52 +0000

    [Client/ Lua] Add API for basic mouse control eg locking or hiding it

--- src/client/lua/client_lua_api.h
+++ src/client/lua/client_lua_api.h
@@ -2,7 +2,7 @@
 
 struct ScriptEngine;
 
-#include <optional>
+#include <SFML/Window/Window.hpp>
 
 namespace gui {
 class OverlayFactory;
@@ -10,8 +10,32 @@ class OverlayStack;
 } // namespace gui
 
 class GuiRenderer;
+struct InputState;
 
+/**
+ * @brief Init the overlay APIs
+ *
+ * @param scriptEngine The Lua script engine/ lua state to append the API to
+ * @param overlayFactory The client's GUI Overlay Factory
+ * @param overlayStack The client's GUI overlay stack
+ * @param guiRenderer The client's GUI Renderer; optional
+ */
 void luaInitGuiApi(ScriptEngine& scriptEngine, gui::OverlayFactory& overlayFactory,
                    gui::OverlayStack& overlayStack, GuiRenderer* guiRenderer = nullptr);
 
+/**
+ * @brief Init API for the various Open Builder widgets, (Buttons, etc)
+ *
+ * @param scriptEngine The Lua script engine/ lua state to append the API to
+ */
 void luaInitGuiWidgetApi(ScriptEngine& scriptEngine);
+
+/**
+ * @brief
+ *
+ * @param scriptEngine The Lua script engine/ lua state to append the API to
+ * @param window The window used for certain control (eg locking or showing mouse)
+ * @param inputState The input state for furthur input control
+ */
+void luaInitInputApi(ScriptEngine& scriptEngine, sf::Window& window,
+                     InputState& inputState);
