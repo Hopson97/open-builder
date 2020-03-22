1f1149fb44d7dd07a20856246e6d04ad6590af40
Hopson97 <mhopson@hotmail.co.uk>
2020-03-22 10:29:01 +0000

    Move some files into common util folder

---
+++ src/common/common/util/string_data_mapper.cpp
@@ -0,0 +1,11 @@
+#include "string_data_mapper.h"
+
+void StringDataMapper::parse(const std::string& data)
+{
+
+}
+
+std::string StringDataMapper::findValue(const std::string& key)
+{
+    return std::string();
+}
