c133f4bbf11a982aaeacdd346fa0c901d4e5e802
Hopson97 <mhopson@hotmail.co.uk>
2020-03-22 21:38:42 +0000

    Remove string data mapper

--- src/common/common/lua/util_api.cpp
+++ src/common/common/lua/util_api.cpp
@@ -1,7 +1,6 @@
 #include "script_engine.h"
 
-#include "../util//random_number_generator.h"
-#include "../util/string_data_mapper.h"
+#include "../util/random_number_generator.h"
 
 namespace {
     void addRandomNumberGeneratorApi(ScriptEngine& scriptEngine)
@@ -11,19 +10,9 @@ namespace {
         rngApi["nextInt"] = &RandomNumberGenerator::nextInt;
         rngApi["nextFloat"] = &RandomNumberGenerator::nextFloat;
     }
-
-    void addStringDataMapperApi(ScriptEngine& scriptEngine)
-    {
-        auto dataMapperApi =
-            scriptEngine.lua.new_usertype<StringDataMapper>("StringDataMapper");
-        dataMapperApi["getString"] = &StringDataMapper::getString;
-        dataMapperApi["getInt"] = &StringDataMapper::getInt;
-        dataMapperApi["getFloat"] = &StringDataMapper::getFloat;
-    }
 } // namespace
 
 void ScriptEngine::luaInitUtilApi()
 {
     addRandomNumberGeneratorApi(*this);
-    addStringDataMapperApi(*this);
 }
