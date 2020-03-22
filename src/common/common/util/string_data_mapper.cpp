177924126487807f25103975a816085b59bd3c84
Hopson97 <mhopson@hotmail.co.uk>
2020-03-22 19:27:32 +0000

    Add basic parser class

--- src/common/common/util/string_data_mapper.cpp
+++ src/common/common/util/string_data_mapper.cpp
@@ -1,11 +1,31 @@
 #include "string_data_mapper.h"
 
+#include <sstream>
+
+namespace {
+
+    std::vector<std::string> split(const std::string input, char delim)
+    {
+        std::vector<std::string> strings;
+        std::stringstream stream(input);
+        std::string line;
+        while (std::getline(stream, line, delim)) {
+            strings.push_back(line);
+        }
+        return strings;
+    }
+} // namespace
+
 void StringDataMapper::parse(const std::string& data)
 {
-
+    auto pairs = split(data, ';');
+    for (const auto& pair : pairs) {
+        auto keyValue = split(pair, '=');
+        m_parsedData.emplace(keyValue[0], keyValue[1]);
+    }
 }
 
 std::string StringDataMapper::findValue(const std::string& key)
 {
-    return std::string();
+    return m_parsedData[key];
 }
