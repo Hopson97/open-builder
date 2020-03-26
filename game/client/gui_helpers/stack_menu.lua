c006d4a329c72fc2de7091296c7e05a1cd105b50
BinaryDrag0n <54193028+BinaryDrag0n@users.noreply.github.com>
2020-03-26 14:14:05 -0400

    Add files via upload

--- game/client/gui_helpers/stack_menu.lua
+++ game/client/gui_helpers/stack_menu.lua
@@ -2,6 +2,8 @@ local WIDGET_WIDTH = 800
 local WIDGET_HEIGHT = 90
 local BUTTON_SIZE = GuiDim.new(0, WIDGET_WIDTH, 0, WIDGET_HEIGHT)
 local widgetTexture = game.gui.getTexture("res/button.png")
+local checkboxTexture_unchecked = game.gui.getTexture("res/checkbox-unchecked.png")
+local checkboxTexture_checked = game.gui.getTexture("res/checkbox-checked.png")
 
 StackMenu = {}
 StackMenu.__index = StackMenu
@@ -17,20 +19,78 @@ function StackMenu:create(yStart, overlay, gap, title)
     vals.overlay = overlay
     vals.widgetGap = WIDGET_HEIGHT + gap
     if title ~= nil then
-        local label = createTitle(overlay, title, vals.y)
+        local label = overlay:addCenteredLabel()
+        label.position = GuiDim.new(0, 0, 0, vals.y)
+        label.text = title
+        label.textSize = 100
         vals.y = vals.y - 140
     end
     return vals
 end
 
-function StackMenu:nextWidgetPosition()
-    local position = getWidgetCentre(WIDGET_WIDTH, self.y)
-    self.y = self.y - self.widgetGap
+function StackMenu:nextWidgetPosition(width, centered, x, y)
+    local position
+    if(centered) then 
+        position = getWidgetCentre(width, self.y - y)
+    else
+        position = GuiDim.new(0, (1920 / 2 - width / 2) + x, 0, self.y - y)
+    end
+    self.y = (self.y + y) - self.widgetGap
     return position
 end
 
 function StackMenu:setBackground(image)
-    return createBackground(self.overlay, image)
+    local background = self.overlay:addImage()
+    background.size = GuiDim.new(1.0, 0, 1.0, 0)
+    background.image = image
+    return background
+end
+
+function StackMenu:initBasicWidget(widget, x, y, w, h, keepAlignment, texture, textSize)
+    if(keepAlignment) then
+        -- center the button, and place it in sequence through the nextWidgetPosition function -- 
+        position = self:nextWidgetPosition(w, true, 0, 0)
+        widget.size = GuiDim.new(0, w, 0, h)
+        widget.position = position
+        
+        if(texture ~= nil) then
+            widget.image = texture
+        end
+
+        if(textSize ~= nil) then
+            widget.textSize = textSize
+        end   
+
+        widget.onMouseOver = function()
+            widget:setColour(1.25, 1.25, 1.25)
+        end
+
+        widget.onMouseOff = function()
+            widget:setColour(1, 1, 1)
+        end
+    else 
+        -- use the x, y values as modifiers --
+        position = self:nextWidgetPosition(w, false, x, y)
+        widget.size = GuiDim.new(0, w, 0, h)
+        widget.position = position
+
+        if(texture ~= nil) then
+            widget.image = texture
+        end
+
+        if(textSize ~= nil) then
+            widget.textSize = textSize
+        end
+
+        widget.onMouseOver = function()
+            widget:setColour(1.25, 1.25, 1.25)
+        end
+
+        widget.onMouseOff = function()
+            widget:setColour(1, 1, 1)
+        end
+    end
+
 end
 
 function StackMenu:addImage(texture, width, height)
@@ -42,18 +102,10 @@ function StackMenu:addImage(texture, width, height)
     return image
 end
 
-function StackMenu:initBasicWidget(widget)
-    widget.size = BUTTON_SIZE
-    widget.position = self:nextWidgetPosition()
-    widget.textSize = 50
-    widget.image = widgetTexture
-    setHighlightOnMouseOver(widget)
-end
-
 function StackMenu:addTextBox(label, placeholder)
     self:pad(25)
     local textBox = self.overlay:addTextBox()
-    self:initBasicWidget(textBox)
+    self:initBasicWidget(textBox, 0, 0, WIDGET_WIDTH, WIDGET_HEIGHT, true, widgetTexture, 50)
     textBox.placeholder = placeholder
     textBox.label = label
     textBox.maxLength = 25
@@ -63,26 +115,24 @@ end
 
 function StackMenu:addButton(text)
     local button = self.overlay:addButton()
-    self:initBasicWidget(button)
+    self:initBasicWidget(button, 0, 0, WIDGET_WIDTH, WIDGET_HEIGHT, true, widgetTexture, 50)
     button.text = text
 
     return button
 end
 
-function StackMenu:addLabel(text)
-    local label = self.overlay:addLabel()
-    label.textSize = 100
-    label.position = self:nextWidgetPosition()
-    label.text = text
-    return label
+function StackMenu:addCheckBox(text)
+    local checkBox = self.overlay:addCheckBox()
+    self:initBasicWidget(checkBox, -250, 0, WIDGET_WIDTH - 500, WIDGET_HEIGHT, false, checkboxTexture_unchecked, 30)
+    checkBox.text = text
+    return checkBox
 end
 
-function StackMenu:addBackButton()
-    local backButton = self:addButton("Back")
-    backButton.onClick = function()
-        game.gui.pop()
-    end
-    return backButton
+function StackMenu:addLabel(text)
+    local label = self.overlay:addLabel()
+    self:initBasicWidget(label, 0, 0, WIDGET_WIDTH, WIDGET_HEIGHT, true, widgetTexture, 100)
+    label.text = text
+    return label
 end
 
 function StackMenu:pad(amount)
