62a2ada3d91a7fac1b4d97a70b206e27f7b35c76
Matthew Hopson <matts.hopson@gmail.com>
2020-03-27 23:11:02 +0000

    Revert "Make the shader compiler error out more verbosely"
    This reverts commit 3317e6cf08a3b64b623320b32ddd2bf465853659.

--- src/common/common/util/file_io.h
+++ src/common/common/util/file_io.h
@@ -1,11 +1,10 @@
 #pragma once
 
 #include <string>
-#include <string_view>
 
 /**
  * @brief Loads the entire content from a text file
  * @param path The path of the file to load
  * @return std::string Contents of the file
  */
-std::string loadFileContents(const std::string_view path);
+std::string loadFileContents(const std::string& path);
