b201e5b5edd9790c335f9b0587ea66f98710a261
Matthew Hopson <mhopson@hotmail.co.uk>
2020-04-09 22:06:51 +0100

    Nope - Its time to start fresh... (#197)
    * Nope
    
    * Updated the readme

--- shaders/selection_fragment.glsl
+++
@@ -1,7 +0,0 @@
-#version 330
-
-out vec4 outColour;
-
-void main() {
-    outColour = vec4(0, 0, 0, 0.5);
-}
--- src/client/input/input_state.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-// @TODO, prob a better way to do this
-struct InputState {
-    bool isMouseLocked = false;
-};
